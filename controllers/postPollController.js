19ad5c71ce19e826c04f840cb344c0b9d59b4eda
LucasZuq <lucasdeozuquetto@hotmail.com>
2022-09-21 11:20:45 -0300

    if expireAt atributte be empty, replace to a default value that is 30 days after now

--- controllers/postPollController.js
+++ controllers/postPollController.js
@@ -1,13 +1,13 @@
-import db from '../src/db';
+import db from "../src/db.js";
 
-export async function postPollController (req,res) {
-    const pollObject = req.body
-    
-    try {
-        await db.collection('polls').insertOne(pollObject)
-        res.status(201).send()
-    } catch (error) {
-        console.log(error.message)
-        res.sendStatus(500)
-    }
+export async function postPollController(req, res) {
+   const pollObject = req.body;
+
+   try {
+      await db.collection("polls").insertOne(pollObject);
+      res.status(201).send(pollObject);
+   } catch (error) {
+      console.log(error.message);
+      res.sendStatus(500);
+   }
 }
--- middlewares/pollMiddleware.js
+++ middlewares/pollMiddleware.js
@@ -1,18 +1,24 @@
-import Joi from "joi"
+import Joi from "joi";
+import dayjs from "dayjs";
 
-export default function pollMiddleware(req,res,next){
-    const pollObject = req.body
 
-    const pollSchema = Joi.object({
-        title:Joi.string().min(1).required()
-    })
-    const validate = pollSchema.validate(pollObject)
+export default function pollMiddleware(req, res, next) {
+   const pollObject = req.body;
+   const after30days = dayjs().add(30, "day").format("YYYY-MM-DD HH:mm");
+   const pollSchema = Joi.object({
+      title: Joi.string().min(1),
+      expireAt: Joi.string().allow("", null).required(),
+   });
+   const validate = pollSchema.validate(pollObject, { abortEarly: false });
 
-    if(validate.error){
-        console.error(validate.error)
-        res.sendStatus(422)
-        return
-    }
+   if (validate.error) {
+      console.error(validate.error.details.map((error) => error));
+      res.sendStatus(422);
+      return;
+   }
+   if (pollObject.expireAt.length === 0) {
+      pollObject.expireAt = after30days;
+   }
 
-    next()
+   next();
 }
